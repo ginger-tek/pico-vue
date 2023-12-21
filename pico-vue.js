73166f296312a8f45b950a3747ec62697d7d3411
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2023-12-20 21:47:35 -0500

    Update pico-vue.js

--- pico-vue.js
+++ pico-vue.js
@@ -274,17 +274,26 @@ export const NavBar = {
     </ul>
     <ul class="mobile-menu">
       <li>
-        <details role="list" dir="rtl">
+        <details role="list" dir="rtl" ref="menu">
           <summary aria-haspopup="listbox" role="link" class="menu-btn">
             <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="1.2rem" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
           </summary>
-          <ul role="listbox" dir="ltr">
+          <ul role="listbox" dir="ltr" @click="closeMenu">
             <slot name="menu"></slot>
           </ul>
         </details>
       </li>
     </ul>
-  </nav>`
+  </nav>`,
+  setup() {
+    const menu = Vue.ref(null)
+    
+    function closeMenu() {
+      menu.value.removeAttribute('open')
+    }
+    
+    return { menu, closeMenu }
+  }
 }
 
 export const Tab = {
